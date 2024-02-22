41f2e6ed454e7124d49765a89732a89aa11fbc2e
Olorunfemi Davis <oa.davis@outlook.com>
2024-02-22 13:01:43 +0100

    Improve TransactionResponseModel.cs

--- PayStackDotNetSDK/Models/Transactions/TransactionResponseModel.cs
+++ PayStackDotNetSDK/Models/Transactions/TransactionResponseModel.cs
@@ -37,6 +37,8 @@ namespace PayStackDotNetSDK.Models.Transactions
 
     public class Authorization
     {
+        public string brand { get; set; }
+        public string account_name { get; set; }
         public string authorization_code { get; set; }
         public string card_type { get; set; }
         public string last4 { get; set; }
@@ -52,6 +54,11 @@ namespace PayStackDotNetSDK.Models.Transactions
 
     public class Customer
     {
+        public string phone { get; set; }
+        public Metadata metadata { get; set; }
+        public string risk_action { get; set; }
+        public object international_format_phone { get; set; }
+        
         public int id { get; set; }
         public string customer_code { get; set; }
         public string first_name { get; set; }
@@ -61,6 +68,23 @@ namespace PayStackDotNetSDK.Models.Transactions
 
     public class Data
     {
+        public long id { get; set; }
+        public string receipt_number { get; set; }
+        public object paid_at { get; set; }
+        public DateTime created_at { get; set; }
+        public Fees_Split fees_split { get; set; }
+        public object plan { get; set; }
+        //public Split split { get; set; }
+        public object order_id { get; set; }
+        public object paidAt { get; set; }
+        public DateTime createdAt { get; set; }
+        public int requested_amount { get; set; }
+        public object pos_transaction_data { get; set; }
+        public object source { get; set; }
+        public object fees_breakdown { get; set; }
+        //public Plan_Object plan_object { get; set; }
+        public Subaccount subaccount { get; set; }
+        
         public int amount { get; set; }
         public string currency { get; set; }
         public DateTime transaction_date { get; set; }
@@ -76,5 +100,38 @@ namespace PayStackDotNetSDK.Models.Transactions
         public object fees { get; set; }
         public Authorization authorization { get; set; }
         public Customer customer { get; set; }
+    }
+    
+    public class Subaccount
+    {
+        public int id { get; set; }
+        public string subaccount_code { get; set; }
+        public string business_name { get; set; }
+        public string description { get; set; }
+        public object primary_contact_name { get; set; }
+        public object primary_contact_email { get; set; }
+        public object primary_contact_phone { get; set; }
+        public object metadata { get; set; }
+        public int percentage_charge { get; set; }
+        public string settlement_bank { get; set; }
+        public int bank_id { get; set; }
+        public string account_number { get; set; }
+        public string currency { get; set; }
+        public bool active { get; set; }
+    }
+    
+    public class Fees_Split
+    {
+        public int paystack { get; set; }
+        public int integration { get; set; }
+        public int subaccount { get; set; }
+        public Fees_Split_Params _params { get; set; }
+    }
+
+    public class Fees_Split_Params
+    {
+        public string bearer { get; set; }
+        public string transaction_charge { get; set; }
+        public string percentage_charge { get; set; }
     }
 }
